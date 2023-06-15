e2245554a69228e62fe0cad9fbf3421c082bf071
Karen Sarria <karen.dani16@gmail.com>
2023-06-15 12:39:12 -0500

    function getPairs was created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [list,setList]= useState([])
+  const [sum,setSum]= useState()
+  const [pairs,setPairs]= useState()
+  const app=()=>{
+   const result=[]
+   const temp= new Map()
+   for(let i=0;  i<list.length; i++){
+        const findNumber=sum-list[i];
+        if(temp.has(list[i])){
+            result.push(`${temp.get(list[i])},${list[i]}`)
+        }else{
+            temp.set(findNumber, list[i])
+        }  
+   }
+   setPairs(result)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <label>
+          List:
+          <input data-testid='inputList' placeholder='Comma separated list of integers' type="text" name="list" onChange={(e)=>setList(e.currentTarget.value.split(',').map((el)=>parseInt(el)))}/>
+        </label>
+        <label>
+          Sum:
+          <input data-testid='inputSum' placeholder='target sum' type="text" name="sum" onChange={(e)=>setSum(parseInt(e.currentTarget.value))}/>
+        </label>
+      <button data-testid='button'  type='submit' onClick={app}>Submit</button>
+      {pairs?.length>0 ?
+        <div className="list">Pairs 
+          {pairs.map((pair,index)=><div key={`key-${pair}-${index}`}>{pair}</div>)}
+        </div>
+        :<p>Not Pairs</p>
+        }
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act,fireEvent} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('Test Case 1', async() => {
+    render(<App />);
+    await act(async () => {
+        const inputList =screen.getByTestId("inputList");
+        const inputSum =screen.getByTestId("inputSum");
+         fireEvent.change(inputList, {
+                target: { value: "1,9,5,0,20,-4,12,16,7" }
+        })
+        fireEvent.change(inputSum, {
+                target: { value: "12" }
+        })
+        screen.getByTestId("button").click()
+    })
+    expect(screen.getByText("0,12")).toBeInTheDocument();
+    expect(screen.getByText("-4,16")).toBeInTheDocument();
+     expect(screen.getByText("5,7")).toBeInTheDocument();
+  });
+
+  it('Test Case 2', async() => {
+    render(<App />);
+    await act(async () => {
+        const inputList =screen.getByTestId("inputList");
+        const inputSum =screen.getByTestId("inputSum");
+         fireEvent.change(inputList, {
+                target: { value: "1,2,3,4,5,6,7" }
+        })
+        fireEvent.change(inputSum, {
+                target: { value: "10" }
+        })
+        screen.getByTestId("button").click()
+    })
+    expect(screen.getByText("3,7")).toBeInTheDocument();
+    expect(screen.getByText("4,6")).toBeInTheDocument();
+  });
+
+  it('Test Case 3', async() => {
+    render(<App />);
+    await act(async () => {
+        const inputList =screen.getByTestId("inputList");
+        const inputSum =screen.getByTestId("inputSum");
+         fireEvent.change(inputList, {
+                target: { value: "-1,-2,-3,-4,-5,-6,-7" }
+        })
+        fireEvent.change(inputSum, {
+                target: { value: "-10" }
+        })
+        screen.getByTestId("button").click()
+    })
+    expect(screen.getByText("-3,-7")).toBeInTheDocument();
+    expect(screen.getByText("-4,-6")).toBeInTheDocument();
+  });
